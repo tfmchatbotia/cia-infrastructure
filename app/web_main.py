fc107aa3c3381c55c773fc1993980e3786788373
pedroPRUEBA1 <pjmelgar@gmail.com>
2025-05-12 12:04:51 +0200

    actualizado .env

---
+++ app/web_main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/chat", response_class=HTMLResponse)
+async def chat(request: Request):
+    return templates.TemplateResponse("chat.html", {"request": request})
